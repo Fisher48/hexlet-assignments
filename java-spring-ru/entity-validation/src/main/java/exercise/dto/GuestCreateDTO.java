02fe51ee3ad39a0202c09ec0ace03e170618d13e
@hexlet/cli <support@hexlet.io>
2024-10-18 20:28:56 +0300

    submit java-spring-ru/entity-validation

--- java-spring-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
+++ java-spring-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
@@ -1,7 +1,6 @@
 package exercise.dto;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +10,16 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class GuestCreateDTO {
+
+    @NotNull(message = "Имя пользователя должно быть не пустым")
+    private String name;
+
+    @Email(message = "Электронная почта должна быть валидной")
+    private String email;
+
+    @Size(min = 11, max = 13)
+    @Pattern(regexp = "\\G[+][\\d]{11,13}", message = "Номер телефона должен начинаться с символа + и содержать от 11 до 13 цифр")
+    private String phoneNumber;
 
     @Pattern(regexp = "[\\d]{4}", message = "Номер клубной карты должен состоять ровно из четырех цифр")
     private String clubCard;
