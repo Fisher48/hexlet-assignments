0aeed70cc7614454e6c5af497054e9b244cabac5
@hexlet/cli <support@hexlet.io>
2024-10-19 17:48:08 +0300

    submit java-spring-ru/search

--- java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
+++ java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
@@ -21,38 +21,32 @@ public class ProductSpecification {
                 .and(greaterThan(params.getPriceGt()))
                 .and(lessThan(params.getPriceLt()))
                 .and(rateGreaterThan(params.getRatingGt()))
-                .and(betweenPrice(params.getPriceLt(), params.getPriceGt()))
                 .and(contTitle(params.getTitleCont()));
     }
 
     public Specification<Product> withCategoryId(Long categoryId) {
         return ((root, query, cb) -> categoryId == null ? cb.conjunction() :
-                cb.in(root.get("category").get("id")));
+                cb.equal(root.get("category").get("id"), categoryId));
     }
 
-    public Specification<Product> greaterThan(Integer price) {
-        return ((root, query, cb) -> price == null ? cb.conjunction() :
-                cb.greaterThan(root.get("priceBt"), price));
+    public Specification<Product> greaterThan(Integer priceLt) {
+        return ((root, query, cb) -> priceLt == null ? cb.conjunction() :
+                cb.greaterThan(root.get("price"), priceLt));
     }
 
-    public Specification<Product> lessThan(Integer price) {
-        return ((root, query, cb) -> price == null ? cb.conjunction() :
-                cb.lessThan(root.get("priceLt"), price));
+    public Specification<Product> lessThan(Integer priceGt) {
+        return ((root, query, cb) -> priceGt == null ? cb.conjunction() :
+                cb.lessThan(root.get("price"), priceGt));
     }
 
-    public Specification<Product> rateGreaterThan(Double rating) {
-        return ((root, query, cb) -> rating == null ? cb.conjunction() :
-                cb.greaterThan(root.get("ratingGt"), rating));
+    public Specification<Product> rateGreaterThan(Double ratingGt) {
+        return ((root, query, cb) -> ratingGt == null ? cb.conjunction() :
+                cb.greaterThan(root.get("rating"), ratingGt));
     }
 
-    public Specification<Product> betweenPrice(Integer priceBt, Integer priceLt) {
-        return ((root, query, cb) -> priceBt == null || priceLt == null ? cb.conjunction() :
-                cb.between(root.get("priceLt"), priceLt, priceBt));
-    }
-
-    public Specification<Product> contTitle(String title) {
-        return ((root, query, cb) -> title == null ? cb.conjunction() :
-                cb.like(root.get("titleCont"), title));
+    public Specification<Product> contTitle(String titleCont) {
+        return ((root, query, cb) -> titleCont == null ? cb.conjunction() :
+                cb.like(root.get("title"), titleCont.toLowerCase()));
     }
 }
 // END
